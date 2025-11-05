f13b67ac60c77560df46a76aa2ac169e38e0fdcf
Frilly Lakumani <frillysportfolio@gmail.com>
2025-11-05 14:55:00 +0700

    Update: Format Tanggal

--- utils/formatTanggal.js
+++ utils/formatTanggal.js
@@ -1,8 +1,12 @@
 export const formatTanggal = (tanggal) => {
     if (!tanggal) return "-";
-    return new Date(tanggal).toLocaleDateString("id-ID", {
+    return new Date(tanggal).toLocaleString("id-ID", {
         day: "numeric",
         month: "long",
         year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        timeZone: "Asia/Jakarta"
     });
 };
